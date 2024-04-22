83b5b5b13c71d6fb0e88c41a8b4746ad2f903b78
cvoluj <berezanskiydaniiil@gmail.com>
2024-04-22 15:12:30 +0300

    add chat domain events + tests

--- app/tests/domain/values/test_messages.py
+++ app/tests/domain/values/test_messages.py
@@ -4,7 +4,7 @@ import pytest
 from domain.entities.messages import Chat, Message
 from domain.exceptions.messages import TitleTooLongException
 from domain.values.messages import Text, Title
-
+from domain.events.messages import NewMessageRecievedEvent
 
 def test_create_message_success_short_text():
     text = Text('test message success')
@@ -45,3 +45,24 @@ def test_add_chat_to_message():
     chat.add_message(message)
     
     assert message in chat.messages
+
+def test_new_message_event():
+    text = Text('Hello World')
+    message = Message(text=text)
+    
+    title = Title('title')
+    chat = Chat(title=title)
+    
+    chat.add_message(message)
+    events = chat.pull_events()
+    pulled_events = chat.pull_events()
+    
+    assert not pulled_events
+    assert len(events) == 1, events
+    
+    new_event = events[0]
+    
+    assert isinstance(new_event, NewMessageRecievedEvent), new_event
+    assert new_event.message_oid == message.oid
+    assert new_event.message_text == message.text.as_generic_type()
+    assert new_event.chat_oid == chat.oid
